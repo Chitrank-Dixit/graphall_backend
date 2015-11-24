29fb1b44fc5918e8d4d363a1fe287dc4aef912be
Chitrank Dixit <chitrankdixit@gmail.com>
2015-11-24 23:06:17 +0530

    user and accounts api's are working fine

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,77 @@
+import json
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import permissions, status, views, viewsets
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from authentication.permissions import IsAccountOwner
+from authentication.serializers import AccountSerializer
 
-# Create your views here.
+class AccountViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = User.objects.all()
+    print queryset
+    serializer_class = AccountSerializer
+    #print "AccountSerializer mein aaa gaya", viewsets.ModelViewSet
+    def get_permissions(self):
+        #print "permissions mein aaa gaya", self.request.method, permissions.SAFE_METHODS
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            #print "In Post"
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
+
+    def create(self, request):
+        #print "create mein aaa gaya", self.serializer_class(data=request.data)," ji"
+        serializer = self.serializer_class(data=request.data)
+        print serializer
+        if serializer.is_valid():
+            print "users mein aa gaya"
+            User.objects.create_user(**serializer.validated_data)
+
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        print request.data
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(views.APIView):
+    print 'In log in view'
+    queryset = User.objects.all()
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+        username = data.get('username', None)
+        password = data.get('password', None)
+        print "In Login", data
+        account = authenticate(username=username, password=password)
+        print account
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+                print 'yeah'
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        logout(request)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
--- graphall/urls.py
+++ graphall/urls.py
@@ -16,6 +16,22 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet, LoginView, LogoutView
+#from graphall.views import IndexView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+    #url('^.*$', IndexView.as_view(), name='index'),
 ]
