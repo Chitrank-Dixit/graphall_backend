22326114ae8cbcdac3555140dc90c45066112012
Chitrank-Dixit <chitrankdixit@gmail.com>
2016-09-25 00:35:27 +0530

    update the staging settings file

--- graphall/settings/staging.py
+++ graphall/settings/staging.py
@@ -7,7 +7,19 @@ DATABASES = {
         'NAME': get_secret('DATABASE_NAME'),
         'USER': get_secret('DATABASE_USER'),
         'PASSWORD': get_secret('DATABASE_PASSWORD'),
-        'HOST': 'graphall-dev.c15tdklda9nm.us-west-2.rds.amazonaws.com',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
+
+# stopped rds due to more money wasted in maintaining this
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': get_secret('DATABASE_NAME'),
+#         'USER': get_secret('DATABASE_USER'),
+#         'PASSWORD': get_secret('DATABASE_PASSWORD'),
+#         'HOST': 'graphall-dev.c15tdklda9nm.us-west-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
